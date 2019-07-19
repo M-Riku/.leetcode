c592d7d89145b72d404408a2c689f12cd7b74572
陆明浩 <luminghao1994@yahoo.co.jp>
2019-07-20 01:17:03 +0900

    submit problem 5

--- 5.longest-palindromic-substring.py
+++ 5.longest-palindromic-substring.py
@@ -5,5 +5,17 @@
 #
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        print(1)
+        if not s: 
+            return s
+
+        res = ""
+        for i in range(len(s)):
+            j = i + 1
+            while j <= len(s) and len(res) <= len(s[i:]):
+                if s[i:j] == s[i:j][::-1] and len(s[i:j]) > len(res):
+                    res = s[i:j]
+                j += 1
+
+        return res
+
 
