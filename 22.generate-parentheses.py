a3886b0053e1b3617681fed671a9e7695aff56ad
陆明浩 <luminghao1994@yahoo.co.jp>
2019-07-23 19:07:44 +0900

    submit problem 22

---
+++ 22.generate-parentheses.py
@@ -0,0 +1,30 @@
+#
+# @lc app=leetcode id=22 lang=python3
+#
+# [22] Generate Parentheses
+#
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        def recur(stack, letter, n):
+            if n == 0:
+                while stack:
+                    letter += ")"
+                    stack.pop()
+                res.append(letter)
+            else:
+                if not stack:
+                    stack.append("(")
+                    recur(stack.copy(), letter+"(", n-1)
+                else:
+                    tmp = stack.copy()
+                    tmp.append("(")
+                    recur(tmp.copy(), letter+"(", n-1)
+                    tmp = stack.copy()
+                    tmp.pop()
+                    recur(tmp.copy(), letter+")", n)
+        
+        res = []
+        recur(res, "", n)
+        res.pop(0)
+        return res
+
