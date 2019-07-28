b5e7c074ba0fd4fe9212cc16493697a349210bad
陆明浩 <luminghao1994@yahoo.co.jp>
2019-07-28 23:03:27 +0900

    optimize problem 9

--- 9.palindrome-number.py
+++ 9.palindrome-number.py
@@ -3,17 +3,43 @@
 #
 # [9] Palindrome Number
 #
+
+# class Solution:
+#     def isPalindrome(self, x: int) -> bool:
+#         if x < 0 :
+#             return False
+#         S = str(x)
+#         start = 0
+#         end = len(S) - 1
+#         while (True):
+#             if (S[start] != S[end]):
+#                 return False
+#             if (end - start <= 1):
+#                 break
+#             start += 1
+#             end -= 1
+#         return True
+
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         if x < 0 :
             return False
-        S = str(x)
-        start = 0
-        end = len(S) - 1
+        nums = []
+        start = 10
         while (True):
-            if (S[start] != S[end]):
+            i = x % start
+            nums.append(i // (start // 10))
+            x -= i
+            if start > x:
+                break
+            start *= 10
+        
+        start = 0
+        end = len(nums) - 1
+        while(True):
+            if nums[start] != nums[end]:
                 return False
-            if (end - start <= 1):
+            if(end - start <= 1):
                 break
             start += 1
             end -= 1
