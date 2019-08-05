6c9a08aefc4e3c24ce809e1650192046a5e79095
M-Riku <luminghao1994@yahoo.co.jp>
2019-08-05 11:51:30 +0900

    Add files via upload
    submit problem 33

---
+++ 33.search-in-rotated-sorted-array.py
@@ -0,0 +1,29 @@
+#
+# @lc app=leetcode id=33 lang=python3
+#
+# [33] Search in Rotated Sorted Array
+#
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            mid = (right + left) // 2
+            
+            if nums[mid] == target:
+                return mid
+            
+            if nums[left] <= nums[mid]:
+                if nums[left] <= target and target < nums[mid]:
+                    right = mid
+                else:
+                    left = mid + 1
+            else:
+                if nums[mid] < target and target <= nums[right]:
+                    left = mid 
+                else:
+                    right = mid
+        
+        return -1
+
+
