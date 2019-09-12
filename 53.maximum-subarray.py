49ca04d24cf5a4e735423aef7bbad5e0e8a0ef1e
M-Riku <luminghao1994@yahoo.co.jp>
2019-09-12 11:35:03 +0900

    modify ignore pattern

---
+++ 53.maximum-subarray.py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode id=53 lang=python3
+#
+# [53] Maximum Subarray
+#
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = [0] * n
+        res = nums[0]
+        dp[0] = nums[0]
+        for i in range(1,n):
+            if dp[i-1] < 0:
+                dp[i] = nums[i]
+            else:
+                dp[i] = dp[i - 1] + nums[i]
+            res = max(res, dp[i])
+        return res
+
