a182fd02447ef4218a03660f3d55aeefe8c953ad
M-Riku <luminghao1994@yahoo.co.jp>
2019-10-09 16:56:41 +0900

    submit problem 69

---
+++ 69.sqrt-x.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=69 lang=python3
+#
+# [69] Sqrt(x)
+#
+
+# @lc code=start
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        i = 0
+        while True:
+            if i * i > x:
+                break
+            i += 1
+        return i - 1
+        
+# @lc code=end
+
